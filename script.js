d50cb2b829a5b1528975c4422ae95b813347e949
Sean Wines <seanwinesdev@gmail.com>
2016-12-13 19:52:36 -0700

    Initial commit and basic logic

---
+++ script.js
@@ -0,0 +1,71 @@
+console.log('TODOS');
+
+const yeah = function () {
+  console.log('YES!');
+}
+
+const todos = {
+  list: [{item: 'item 1', complete: false}, {item: 'item 2', complete: true}, {item: 'item 3', complete: false}],
+
+  addTodo: function(item, complete = false) {
+    this.list.push({
+      item: item,
+      complete: complete
+    });
+    this.displayTodos();
+    yeah();
+  },
+
+  displayTodos: function() {
+    if(this.list.length > 0) {
+      for (var i = 0; i < this.list.length; i++) {
+        if (this.list[i].complete == true) {
+          console.log('(x) ' + this.list[i].item);
+        } else {
+          console.log('( ) ' + this.list[i].item);
+        }
+      }
+    } else {
+      console.log('Nothing todo');
+    }
+
+  },
+
+  changeTodo: function(index, newItem) {
+    this.list[index].item = newItem;
+    this.displayTodos();
+  },
+
+  deleteTodos: function(index) {
+    this.list.splice(index,1);
+    this.displayTodos();
+  },
+
+  toggleComplete: function(index) {
+    var x = this.list[index];
+    x.complete = !x.complete;
+    this.displayTodos();
+  },
+
+  toggleAll: function() {
+    var totaltodos = this.list.length;
+    var completedTodos = 0;
+
+    for (var i = 0; i < totaltodos; i++) {
+      if (this.list[i].complete === true) {
+        completedTodos++;
+      }
+    }
+
+    for (var i = 0; i < totaltodos; i++) {
+      if (totaltodos === completedTodos) {
+        this.list[i].complete = false;
+      } else {
+        this.list[i].complete = true;
+      }
+    }
+
+    console.log('Total ' + completedTodos);
+    this.displayTodos();
+  }
+}
